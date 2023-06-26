071f7586c0c9022f974a7d56345b56dc36eac2af
EmanuelaPau <uapame@gmail.com>
2023-06-26 14:34:18 +0200

    Add footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,31 @@
 <template>
-    <div>
-        <h1>AppFooter</h1>
+    <div class="container-fluid">
+        <div class="row my_container">
+            <div class="col-6">
+                <ul class="d-flex flex-wrap list-unstyled">
+                    <a class="text-reset text-decoration-none" href="">
+                        <li class="me-4">Term and Conditions</li>
+                    </a>
+                    <a class="text-reset text-decoration-none" href="">
+                        <li class="me-4">Policy</li>
+                    </a>
+                    <a class="text-reset text-decoration-none" href="">
+                        <li class="me-4">About</li>
+                    </a>
+                </ul>
+            </div>
+            <div class="col-6 d-flex flex-wrap justify-content-end px-0">
+                <span class="me-1">
+                    © 2020
+                </span>
+                <span class="txtColorPrimary me-1">
+                    @Themepul
+                </span>
+                <span>
+                    All Rights Reserved.
+                </span>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -13,13 +38,10 @@ export default {
     },
     data() {
         return {
-
             // links: [
             //	'gino',
             //	'ciao',
             //	]
-
-
         }
     },
     methods() {
@@ -27,6 +49,7 @@ export default {
 }		
 </script>
 <style scoped lang="scss">
+@use '../styles/general.scss';
 @use '../styles/partials/_variables.scss';
 @use '../styles/partials/_mixins.scss';
 </style> 
