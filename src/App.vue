e76b596e1b6fce77fa919a299d024e008b6bc303
EmanuelaPau <uapame@gmail.com>
2023-06-26 16:56:04 +0200

    Add dynamic elements to footer

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div class="app">
     <AppHeader />
     <AppMain />
-    <AppFooter />
+    <AppFooter :aboutUs=footer.aboutUs :movieCategory=footer.movieCategory :information=footer.information
+      :recentPosts=footer.recentPosts :footerBottomNav=footer.footerBottomNav :socialIcons=footer.aboutUs.socialIcons />
 
   </div>
 </template>
@@ -18,7 +19,97 @@ export default {
     AppHeader,
     AppMain,
     AppFooter,
-  }
+  },
+  data() {
+    return {
+      footer: {
+        aboutUs: {
+          text: 'Mauris malesuada arcu eu posuere eleifen. Ut egestas tortor at leo egestas tincidunt. Nam tincidunt metus tristique tristique viverra. Mauris erat ante, egestas vitae dapibus seds.',
+          socialIcons: [
+            'fa-brands fa-facebook-f', 'fa-brands fa-twitter', 'fa-brands fa-linkedin-in', 'fa-brands fa-instagram', 'fa-brands fa-pinterest'
+          ],
+          colors: [
+            ''
+          ]
+        },
+        movieCategory: [
+          {
+            name: 'English New Movie',
+            link: ' #'
+          },
+          {
+            name: 'Hindi All Movie',
+            link: '#'
+          },
+          {
+            name: 'Latest Tamil Movie',
+            link: '#'
+          },
+          {
+            name: 'New Top Rating Movie',
+            link: '#'
+          },
+          {
+            name: 'Hornn New movie',
+            link: '#'
+          },
+          {
+            name: 'Animation New Movie',
+            link: '#'
+          },
+
+        ],
+        information: [
+          {
+            name: 'Youtube video Post',
+            link: ' #'
+          },
+          {
+            name: 'Actor Information',
+            link: '#'
+          },
+          {
+            name: 'New Promotion',
+            link: '#'
+          },
+          {
+            name: 'Latest Movie In English',
+            link: '#'
+          },
+          {
+            name: 'Latest Flim Product',
+            link: '#'
+          },
+          {
+            name: 'Video New Video',
+            link: '#'
+          },
+        ],
+        recentPosts: [
+          {
+            img: '../../public/blog-100x100.jpg',
+            title: 'Actor The Concept Movie High Concept Theory see ',
+            date: 'April 14, 2020'
+          },
+          {
+            img: '../../public/blog2-100x100.jpg',
+            title: 'consequat Nam lectus nibh aucibus It has turpis',
+            date: 'April 14, 2020'
+          },
+          {
+            img: '../../public/blog3-100x100.jpg',
+            title: 'Nam lectus nibh consequat aucibus turpis',
+            date: 'April 14, 2020'
+          }
+        ],
+        footerBottomNav: [
+          'Tram And Conditions',
+          'Policy',
+          'About'
+        ]
+      }
+    }
+  },
 }
 </script>
 <style lang="scss">
--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,20 +1,72 @@
 <template>
-    <footer>
-        <AboutUs />
+    <footer class="">
+        <!-- About Us  -->
+        <div class=" ">
+            <div class="container-fluid footer-about">
+                <div class="row my_container">
+                    <div class="col-3 px-3">
+                        <h1 class="my_h1">
+                            About Us
+                        </h1>
+                        <p class="my_p">
+                            Mauris malesuada arcu eu posuere eleifen. Ut egestas tortor at leo egestas tincidunt. Nam
+                            tincidunt metus tristique tristique viverra. Mauris erat ante, egestas vitae dapibus seds.
+                        </p>
+                        <ul class="list-unstyled d-flex flex-wrap">
+                            <li v-for="icon in    socialIcons   " class="me-3 social-icon">
+                                <a :class="icon" href="">
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-3 px-3">
+                        <h1 class="my_h1">
+                            Movie Category
+                        </h1>
+                        <ul class="list-unstyled">
+                            <li v-for="li in movieCategory" class="footer-li">
+                                <a class="text-decoration-none text-reset" href="">{{ li.name }}</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-3 px-3">
+                        <h1 class="my_h1">
+                            Information
+                        </h1>
+                        <ul class="list-unstyled">
+                            <li v-for="li in information" class="footer-li">
+                                <a class="text-decoration-none text-reset" href="">{{ li.name }}</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-3 px-3">
+                        <h1 class="my_h1">
+                            Recent Posts
+                        </h1>
+                        <div v-for="cards in recentPosts" class="footer-card d-flex">
+                            <img :src="cards.img" alt="">
+                            <div class="d-flex flex-wrap align-items-end">
+                                <p class="my_p m-0">{{ cards.title }}</p>
+                                <p class="my_p m-0">{{ cards.date }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
+        </div>
+        <div class="footer-about-back"></div>
+        <!-- Footer bottom  -->
         <div class="container-fluid">
             <div class="row my_container py-3">
                 <div class="col-6">
                     <ul class="d-flex flex-wrap list-unstyled my_nav m-0">
-                        <a class="text-reset text-decoration-none" href="">
-                            <li class="me-4">Term and Conditions</li>
-                        </a>
-                        <a class="text-reset text-decoration-none" href="">
-                            <li class="me-4">Policy</li>
-                        </a>
-                        <a class="text-reset text-decoration-none" href="">
-                            <li class="me-4">About</li>
-                        </a>
+                        <li v-for="   element    in    footerBottomNav   " class="me-4">
+                            <a class="text-reset text-decoration-none" href="">
+                                {{ element }}
+                            </a>
+                        </li>
+
                     </ul>
                 </div>
                 <div class="col-6 d-flex flex-wrap justify-content-end px-0">
@@ -34,12 +86,18 @@
 </template>
 
 <script>
-import AboutUs from './Footer/FooterAboutUs.vue'
 
 export default {
     name: 'AppFooter',
+    props: {
+        aboutUs: 'Object',
+        movieCategory: 'Object',
+        information: 'Object',
+        recentPosts: 'Object',
+        footerBottomNav: 'Object',
+        socialIcons: 'Array',
+    },
     components: {
-        AboutUs
     },
     data() {
         return {
@@ -58,11 +116,51 @@ export default {
 @use '../styles/partials/_variables.scss';
 @use '../styles/partials/_mixins.scss';
 
-.my_nav a li {
+.footer-about {
+    padding-top: 70px;
+    height: 500px;
+    width: 100%;
+    line-height: 24px;
+
+    position: absolute;
+    left: 0;
+}
+
+.footer-li {
+    height: 48px;
+    font-size: 15px;
+}
+
+.footer-card {
+    width: 100%;
+    height: 85px;
+    margin-bottom: 30px;
+
+    img {
+        height: 100%;
+        margin-right: 10px;
+    }
+
+
+}
+
+div.footer-about-back {
+    height: 500px;
+    background-image: url('../../public/ft-bg.jpg');
+    background-size: cover;
+    background-position: center;
+    filter: grayscale(100%);
+    opacity: 0.1;
+
+    position: relative;
+}
+
+
+.my_nav li a {
     transition: all .8s;
 }
 
-.my_nav a li:hover {
+.my_nav li a:hover {
     color: #13be13;
 }
 
@@ -73,5 +171,28 @@ export default {
 
 .my_info a:hover {
     color: white;
+}
+
+h1.my_h1 {
+    font-size: 18px;
+    margin-bottom: 30px;
+}
+
+p.my_p {
+    font-size: 14px;
+}
+
+.social-icon {
+    height: 32px;
+    width: 32px;
+    border-radius: 50%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    a {
+        color: white;
+    }
 }
 </style> 
