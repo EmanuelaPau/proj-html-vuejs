3cc4ffe6cd8cba752bd7973322a87483884b3d91
EmanuelaPau <uapame@gmail.com>
2023-06-27 12:51:06 +0200

    Add dynamic cards

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,7 +5,9 @@
         <MainTitle :title=titles.moviePlaylist.title :subtitle=titles.moviePlaylist.subtitle />
         <MainTitle :title=titles.newMovie.title :subtitle=titles.newMovie.subtitle />
         <MainTitle :title=titles.latestNews.title :subtitle=titles.latestNews.subtitle />
-        <SquareCards />
+        <SquareCardsContainer :bigCardName=squareCards.big.title :bigCardImg=squareCards.big.img
+            :bigCardFont=squareCards.big.fontSize :bigCardDate="squareCards.big.date"
+            :bigCardDisplay="squareCards.big.display" :smallCard=squareCards.small />
         <MainMail />
     </main>
 </template>
@@ -15,14 +17,14 @@
 import MainJumbo from './Main/MainJumbo.vue'
 import MainMail from './Main/MainMail.vue'
 import MainTitle from './Main/MainTitle.vue'
-import SquareCards from './Main/MainSquareCards.vue'
+import SquareCardsContainer from './Main/MainSquareCardsContainer.vue'
 export default {
     name: 'AppMain',
     components: {
         MainMail,
         MainJumbo,
         MainTitle,
-        SquareCards,
+        SquareCardsContainer,
     },
     data() {
         return {
@@ -39,6 +41,45 @@ export default {
                     title: 'Latest News',
                     subtitle: 'Lorem Ipsum is simply dummy text of the printing and typesettin',
                 }
+            },
+            squareCards: {
+                big: {
+                    title: 'Actor The Concept Movie High Concept Theory see the post',
+                    img: 'blog-570x570.jpg',
+                    fontSize: 'my_h1-big',
+                    date: '14 April, 2020',
+                    display: ''
+                },
+                small: [
+                    {
+                        title: 'consequat Nam lectus nibh aucibus It has turpis',
+                        img: 'blog2-570x570.jpg',
+                        fontSize: 'my_h1-small',
+                        date: '14 April, 2020',
+                        display: 'd-none'
+                    },
+                    {
+                        title: 'consequat Nam lectus nibh aucibus It has turpis',
+                        img: 'blog3-570x570.jpg',
+                        fontSize: 'my_h1-small',
+                        date: '14 April, 2020',
+                        display: 'd-none'
+                    },
+                    {
+                        title: 'consequat Nam lectus nibh aucibus It has turpis',
+                        img: 'blog4-570x570.jpg',
+                        fontSize: 'my_h1-small',
+                        date: '14 April, 2020',
+                        display: 'd-none'
+                    },
+                    {
+                        title: 'consequat Nam lectus nibh aucibus It has turpis',
+                        img: 'blog5-570x570.jpg',
+                        fontSize: 'my_h1-small',
+                        date: '14 April, 2020',
+                        display: 'd-none'
+                    },
+                ]
             }
         }
     },
