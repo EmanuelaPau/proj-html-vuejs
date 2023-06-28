2f097221e6491aa31583e1f96022891b8de96e28
EmanuelaPau <uapame@gmail.com>
2023-06-28 17:36:45 +0200

    Add responsivness to slider

--- src/components/Main/MainFullPageSlider.vue
+++ src/components/Main/MainFullPageSlider.vue
@@ -1,15 +1,18 @@
 <template>
     <div class="wrapper marginBottom d-flex justify-content-between align-items-center">
-        <button class="arrows" @click="index < 2 ? index = 2 : index--"><i
+        <button class="arrows d-none d-md-block rotate-flip" @click="index < 2 ? index = 2 : index--"><i
                 class="fi fi-sr-paper-plane-top rotate-flip"></i></button>
-        <div class="info">
-            <p>Welcome To Our Movie Site</p>
-            <h1>Our special <span>movies</span></h1>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industrioy. Lorem Ipsum has been the
-                industry standard dummy text ever since the 1500s, when an unknown</p>
-            <button>Read More</button>
+        <div class="info d-flex justify-content-center justify-content-lg-end">
+            <div class="info-container">
+                <p>Welcome To Our Movie Site</p>
+                <h1>Our special <span>movies</span></h1>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industrioy. Lorem Ipsum has been the
+                    industry standard dummy text ever since the 1500s, when an unknown</p>
+                <button>Read More</button>
+            </div>
         </div>
-        <button class="arrows" @click="index > 1 ? index = 1 : index++"><i class="fi fi-sr-paper-plane-top"></i></button>
+        <button class="arrows d-none d-md-block" @click="index > 1 ? index = 1 : index++"><i
+                class="fi fi-sr-paper-plane-top"></i></button>
         <img :class="index == 2 ? 'opacity-100' : 'opacity-0'" src="slider2.jpg" alt="">
         <img :class="index == 1 ? 'opacity-100' : 'opacity-0'" src="slider.jpg" alt="">
     </div>
@@ -81,14 +84,23 @@ export default {
         color: #13be13;
     }
 
-    .info {
-        z-index: 1;
+    .margin-left {
+        margin-left: 300px;
+    }
+
+    .info-container {
         width: 450px;
         text-align: center;
-        margin-left: 300px;
+
         display: flex;
         flex-direction: column;
         align-items: center;
+    }
+
+    .info {
+        z-index: 1;
+        width: 750px;
+
 
         h1 {
             text-transform: uppercase;
